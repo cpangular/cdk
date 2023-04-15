e4a8abff2747edb46e351a4e90723c446f6171aa
Dan Erdelt <erd@conceptual.ca>
2023-04-15 01:47:44 +0000

    refactor

---
+++ packages/rxjs/src/lib/rxjs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-rxjs',
+  template: `
+    <p>
+      rxjs works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RxjsComponent {
+
+}
---
+++ packages/rxjs/src/lib/rxjs.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RxjsComponent } from './rxjs.component';
+
+
+
+@NgModule({
+  declarations: [
+    RxjsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    RxjsComponent
+  ]
+})
+export class RxjsModule { }
