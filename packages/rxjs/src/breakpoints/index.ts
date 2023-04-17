dfaea5577fd68776edc0063521c3898f2c1bc477
Dan Erdelt <erd@conceptual.ca>
2023-04-17 02:00:53 +0000

    breakpoint definition

--- packages/rxjs/src/breakpoints/index.ts
+++ packages/rxjs/src/breakpoints/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export * from './mapBreakpoints';
 export * from './breakpointValue';
+export * from './ViewportBreakpoints';
--- packages/rxjs/src/breakpoints/types/index.ts
+++ packages/rxjs/src/breakpoints/types/index.ts
@@ -1,2 +1,7 @@
+export * from './BPCondition';
 export * from './BreakpointCondition';
 export * from './BreakpointOperation';
+export * from './BreakpointRange';
+export * from './BreakpointValues';
+export * from './ViewportBreakpointValues';
+
