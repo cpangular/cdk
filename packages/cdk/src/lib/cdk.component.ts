691f70d5351a8116538a6e770d9317a4be9888d6
Erd <erd@conceptual.ca>
2023-10-10 22:22:08 +0000

    scripts

---
+++ packages/cdk/src/lib/cdk.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cpng-cdk',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>cdk works!</p> `,
+  styles: [],
+})
+export class CdkComponent {}
---
+++ packages/components/src/lib/components.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cpng-components',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>components works!</p> `,
+  styles: [],
+})
+export class ComponentsComponent {}
